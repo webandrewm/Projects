bb4bc216fb31a9e0c7a6e9663f47aadeff9c8e89
webandrewm <orposts@gmail.com>
2023-03-09 20:05:10 +0300

    Add files via upload

---
+++ ArticlePublishMySQL/frontend-sql/components/AddArticle.tsx
@@ -0,0 +1,106 @@
+import { useAppDispatch, useAppSelector } from '@/reduxlogic/hooks'
+import {
+  sendArticle,
+  setArtName,
+  setArtText,
+  setAuthor,
+} from '@/reduxlogic/Slices/ArticleSlice'
+import { setToMain } from '@/reduxlogic/Slices/RoutingSlice'
+import { RootState } from '@/reduxlogic/store'
+import s from './AddArticle.module.css'
+import 'react-quill/dist/quill.snow.css'
+import dynamic from 'next/dynamic'
+import React from 'react'
+export const QuillNoSSRWrapper = dynamic(import('react-quill'), {
+  ssr: false,
+  loading: () => <p>Loading</p>,
+})
+const formats = [
+  'header',
+  'align',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'video',
+]
+
+export const modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    [{ align: [] }],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+}
+const AddArticle = () => {
+  const dispatch = useAppDispatch()
+  const { author, articleName, articleText } = useAppSelector(
+    (state: RootState) => state.artdata
+  )
+  const { responseLogin } = useAppSelector((state) => state.auth)
+  return (
+    <div className={s.mainDiv}>
+      <div className={s.textDiv}>
+        <input
+          onChange={(e) => dispatch(setAuthor(e.target.value))}
+          className={s.artInput}
+          placeholder="Автор"
+        ></input>
+
+        <input
+          onChange={(e) => dispatch(setArtName(e.target.value))}
+          className={s.artInput}
+          placeholder="Название статьи"
+        ></input>
+        <QuillNoSSRWrapper
+          style={{ height: '100%', margin: '10px' }}
+          onChange={(e) => {
+            dispatch(setArtText(e))
+          }}
+          modules={modules}
+          formats={formats}
+          theme="snow"
+        />
+
+        <button
+          onClick={async () => {
+            await dispatch(
+              sendArticle({
+                author: author,
+                articleName: articleName,
+                articleText: articleText,
+                token: responseLogin.token,
+              })
+            )
+            dispatch(setToMain())
+          }}
+          className={s.addArtButt}
+        >
+          Добавить статью
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default AddArticle
---
+++ ArticlePublishMySQL/frontend-sql/components/myArticles/MyArticles.tsx
@@ -0,0 +1,82 @@
+import { useAppDispatch, useAppSelector } from '@/reduxlogic/hooks'
+import React from 'react'
+import s from './MyArticles.module.css'
+import {
+  setArtIdForWatch,
+  setEditArticle,
+} from '@/reduxlogic/Slices/RoutingSlice'
+import { getOneArt } from '@/reduxlogic/Slices/ArticleSlice'
+import { setAddArticle } from '@/reduxlogic/Slices/RoutingSlice'
+import { deleteOneArticle } from '@/reduxlogic/Slices/MyArticles'
+import { getMyArt } from '@/reduxlogic/Slices/MyArticles'
+const MyArticles = () => {
+  const dispatch = useAppDispatch()
+  const { myArticles } = useAppSelector((state) => state.myart)
+  const { responseLogin } = useAppSelector((state) => state.auth)
+
+  return (
+    <div className={s.artDiv}>
+      {myArticles.length > 0 ? (
+        myArticles.map((item, index) => {
+          return (
+            <div key={index} className={s.oneArtDiv}>
+              <div
+                onClick={async () => {
+                  dispatch(setArtIdForWatch(item.id))
+                  dispatch(getOneArt(item.id))
+                }}
+                className={s.oneArtModule}
+                key={index}
+              >
+                <h1 style={{ margin: '15px', textAlign: 'center' }}>
+                  {item.articleName}
+                </h1>
+                <p style={{ margin: '15px' }}>Автор: {item.author}</p>
+              </div>
+              {responseLogin.token.length > 10 ? (
+                <div>
+                  <button
+                    onClick={async () => {
+                      await dispatch(
+                        deleteOneArticle({
+                          id: item.id,
+                          token: responseLogin.token,
+                        })
+                      )
+                      await dispatch(getMyArt(responseLogin.token))
+                    }}
+                  >
+                    Удалить
+                  </button>
+                  <button
+                    onClick={async () => {
+                      await dispatch(getOneArt(item.id))
+                      dispatch(setEditArticle())
+                    }}
+                  >
+                    Редактировать
+                  </button>
+                </div>
+              ) : (
+                ''
+              )}
+            </div>
+          )
+        })
+      ) : (
+        <div>
+          <h1>У вас пока нет статей.</h1>
+          <p>Вы можете создать статью прямо сейчас.</p>
+          <button
+            style={{ borderRadius: '5px' }}
+            onClick={() => dispatch(setAddArticle())}
+          >
+            Создать статью
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default MyArticles
